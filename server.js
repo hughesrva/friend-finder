49c48a6e1ea67ac79efe2dd41bbb59da106f4e1f
skiplodem <hughesrva@gmail.com>
2019-02-19 23:36:31 -0500

    adding fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
